1e0070c20efa7f96681bc24698cfd27a55a4fbf6
jana <jana.r.a.droubi@gmail.com>
2026-03-31 18:27:38 +0300

    fix: add missing jest e2e configuration

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -1,6 +1,15 @@
-﻿module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/*.e2e.test.ts'],
+﻿/** @type {import('jest').Config} */
+export default {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  // Increased timeout for E2E tests (30s) as they involve Docker/DB
   testTimeout: 30000,
+  transform: {
+    "^.+\\.ts$": ["ts-jest", { useESM: true }],
+  },
+  // Specifically look for your e2e test files
+  testMatch: ["**/*.e2e.test.ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
